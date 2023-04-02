39c9d1e50b2969e0f147f83ab0fadac125b0a388
talehafandi <alhuseynov.taleh@gmail.com>
2023-04-02 18:15:43 +0400

    fix: ensure unique names

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,7 @@
 const Model = require('../models/product.model');
 const VariantModel = require('../models/variant.model')
 const asyncMiddleware = require('../middlewares/async.middleware');
+const variantModel = require('../models/variant.model');
 
 exports.list = asyncMiddleware(async (_, res) => {
     const products = await Model.find({})
@@ -36,12 +37,17 @@ exports.listByCategory = asyncMiddleware(async (req, res) => {
 });
 
 //variant-group
-//? duplicate names - index it
 exports.createVariantGroup = asyncMiddleware(async (req, res) => {
     const productId = req.params.productId;
     const group = req.body;
+    
+    // to avoid duplication, gonna find better solution
+    const isExist = await Model.findOne({ _id: productId, 'variantGroups.name': group.name })
+    if(isExist) return res.status(400).json({message: 'ITEM_ALREADY_EXISTS'})
+    
 
     const product = await Model.findByIdAndUpdate(productId, { $addToSet: { variantGroups: group } }, { new: true })
+
     res.status(200).json(product)
 });
 
@@ -58,10 +64,14 @@ exports.deleteVariantGroup = asyncMiddleware(async (req, res) => {
 exports.updateVariantGroup = asyncMiddleware(async (req, res) => {
     const data = req.body
 
+    // to avoid duplication, gonna find better solution
+    const isExist = await Model.findOne({ _id: req.params.productId, 'variantGroups.name': data.name })
+    if(isExist) return res.status(400).json({message: 'ITEM_ALREADY_EXISTS'})
+
     const filter = { _id: req.params.productId, "variantGroups._id": req.params.groupId }
     const payload = { $set: {} }
 
-    for (let key in data) payload.$set[`variantGroups.$.${key}`] = req.body[key]
+    for (let key in data) payload.$set[`variantGroups.$.${key}`] = data[key]
 
     const product = await Model.findOneAndUpdate(filter, payload, { new: true })
 
@@ -72,7 +82,7 @@ exports.updateVariantGroup = asyncMiddleware(async (req, res) => {
 //? when product does't have any variant returns response with 404
 exports.listVariants = asyncMiddleware(async (req, res) => {
     const variants = await VariantModel.find({product: req.params.productId})
-    // if(!variants.length) return res.status(404).json({message: "NOT_FOUND"})
+    if(!variants.length) return res.status(404).json({message: "NOT_FOUND"})
 
     res.status(200).json(variants)
 })
@@ -92,24 +102,23 @@ exports.createVariant = asyncMiddleware(async (req, res) => {
     if (!product) return res.status(404).json({ message: "ITEM_NOT_FOUND" });
 
     const groupIndex = product.variantGroups.findIndex((group) => group._id == groupId);
-    // if (groupIndex === -1) return res.status(404).json({ message: "VARIANT_GROUP_NOT_FOUND" });
+    if (groupIndex === -1) return res.status(404).json({ message: "VARIANT_GROUP_NOT_FOUND" });
     
-    // const variantPayload = { ...req.body, product: productId };
-    const variant = await VariantModel.create({ ...req.body, product: productId });
+    // to avoid duplication, gonna find better solution
+    const isExist = await VariantModel.findOne({name: req.body.name})
+    if (isExist) return res.status(400).json({message: "VARIANT_ALREADY_EXISTS"})
 
-    // const newVariant = {
-    //     name: variant.name,
-    //     _id: variant._id,
-    //     price: variant.price,
-    //     stock: variant.stock,
-    // };
+    const variantPayload = { ...req.body, product: productId };
+    const variant = await VariantModel.create(variantPayload);
 
-    product.variantGroups[groupIndex].variants.push({
+    const newVariant = {
         name: variant.name,
         _id: variant._id,
         price: variant.price,
         stock: variant.stock,
-    });
+    };
+
+    product.variantGroups[groupIndex].variants.push(newVariant);
     await product.save();
 
     res.status(200).json(product)
