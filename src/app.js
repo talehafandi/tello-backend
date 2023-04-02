d51b972d168b0029968a9ca13891ddcc33f4e977
talehafandi <alhuseynov.taleh@gmail.com>
2023-04-02 15:45:40 +0400

    refactor: app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
-app.use(function (_, res, next) {
+app.use((_, res, next) => {
     res.status(404).send({ message: 'PAGE_NOT_FOUND' })
     next()
 })
