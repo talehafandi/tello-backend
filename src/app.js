646d0e997a1e21459419b13258c3d281999e3bac
talehafandi <alhuseynov.taleh@gmail.com>
2023-04-14 23:33:53 +0400

    feat: access middleware, helmet, rate-limiter

--- src/app.js
+++ src/app.js
@@ -4,16 +4,29 @@ const mongoose = require('mongoose');
 const routes = require('./routes/');
 const cors = require('cors');
 const config = require('./config');
+const access = require('./middlewares/access.middleware')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 app.use(cors())
+// app.use(rateLimit(limiter))
 app.use(express.json())
+app.use(helmet())
+app.use(access)
+
 app.use('/api', routes)
 
 app.use((_, res, next) => {
     res.status(404).send({ message: 'PAGE_NOT_FOUND' })
     next()
 })
-
 
 const PORT = config.port || 6006;
 const DB = config.db.uri.replace("<password>", config.db.pass)
---
+++ src/middlewares/access.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('../utils/jwt')
+
+const bypass = [
+    '/api/v1/auth/login',
+    '/api/v1/auth/signup',
+    '/api/v1/auth/change-password',
+    '/api/v1/google/url',
+    '/api/v1/google/auth',
+    '/api/v1/auth/forgot-password',
+    '/api/v1/auth/forgot-password/confirm',
+]
+
+module.exports = (req, res, next) => {
+    console.log(req.path, bypass.includes(req.path))
+    if (bypass.includes(req.path)) return next()
+    
+    let token = req.headers.authorization || (req.cookie?.token)
+    if (!token) return res.status(403).send({ message: 'Token not provided' })
+
+    try {
+        req.user = jwt.validate(token)
+        next()
+    } catch (e) {
+        return res.status(403).send({ message: 'INVALID_TOKEN' })
+    }
+}
