7231c8343d8b68dfbaa09b74f6c8b5b2c21724fc
talehafandi <alhuseynov.taleh@gmail.com>
2023-06-04 00:04:18 +0400

    feat: add error handler to access middleware

--- src/middlewares/access.middleware.ts
+++ src/middlewares/access.middleware.ts
@@ -1,7 +1,9 @@
-import jwt from'../utils/jwt'
-import User from'../models/user.model'
+import { ApiError } from './../error/ApiError';
+import jwt from '../utils/jwt'
+import User from '../models/user.model'
 import { Response, NextFunction } from 'express';
 import { Req } from '../types/express'
+import asyncMiddleware from './async.middleware';
 
 const bypass = [
     '/api/v1/auth/login',
@@ -13,21 +15,23 @@ const bypass = [
     '/api/v1/auth/forgot-password/confirm',
 ]
 
-export default async (req: Req, res: Response, next: NextFunction) => {
+const access = asyncMiddleware(async (req: Req, res: Response, next: NextFunction) => {
     console.log(req.path, bypass.includes(req.path))
     if (bypass.includes(req.path)) return next()
-    
+
     let token = req.headers.authorization || (req.cookies?.token)
-    if (!token) return res.status(403).send({ message: 'Token not provided' })
+    if (!token) throw new ApiError('TOKEN_NOT_PROVIDED', 401)
 
     try {
         req.user = jwt.verify(token)
         const user = await User.findById(req.user.id)
-        if(!user) return res.status(404).send({ message: 'USER_NOT_FOUND' })
+        if (!user) return res.status(404).send({ message: 'USER_NOT_FOUND' })
         next()
     } catch (e: any) {
         let message = 'INVALID_TOKEN'
-        if(e.message == 'jwt expired') message = 'TOKEN_EXPIRED'
+        if (e.message == 'jwt expired') message = 'TOKEN_EXPIRED'
         return res.status(403).send({ message })
     }
-}
+})
+
+export default access
